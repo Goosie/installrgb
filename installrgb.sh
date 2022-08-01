c44869239b56f9352063b6da8a9343c191ea17a6
Perry Smit <perry.smit@gmail.com>
2022-08-01 10:08:11 +0200

    Update installrgb.sh
    Comment lnp_node gives an error

--- installrgb.sh
+++ installrgb.sh
@@ -1,3 +1,13 @@
+#WIll install all of the rgb nodes and cli dependencies
+
+#Install by:
+#Create dir where you want this, for example
+#mkdir repos/myrepos
+#git clone https://github.com/Goosie/installrgb.git
+#cd installrgb
+#chmod +x installrgb.sh
+#Run with: ./installrgb.sh
+
 cargo install descriptor-wallet --version "0.8.2" --force
 cargo install bp-core --version "0.8.0" --force
 cargo install rgb-std --version "0.8.0" --force
@@ -12,7 +22,7 @@ cargo install store-cli --version "0.8.0" --force
 ```
 cargo install bp_node --version "0.8.0-alpha.2" --force
 #NOT OKE# cargo install lnp_node --path . --locked --all-features --version "0.$
-cargo install lnp_node --version "0.8.0" --force 
+#NOT OKE# cargo install lnp_node --version "0.8.0" --force 
 cargo install rgb_node --version "0.8.0-rc.1" --force
 cargo install storm_node --version "0.8.0-beta.1" --force
 cargo install store_daemon --version "0.8.0" --force
