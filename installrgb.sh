93b9a06213d72368530aeabf62582b48035ca8c6
Perry Smit <perry.smit@gmail.com>
2022-08-01 09:59:14 +0200

    Create installrgb.sh

---
+++ installrgb.sh
@@ -0,0 +1,18 @@
+cargo install descriptor-wallet --version "0.8.2" --force
+cargo install bp-core --version "0.8.0" --force
+cargo install rgb-std --version "0.8.0" --force
+cargo install rgb20 --version "0.8.0-rc.3" --force
+cargo install bp-cli --version "0.8.0-alpha.2" --force
+cargo install lnp-cli --version "0.8.0" --force
+cargo install rgb-cli --version "0.8.0-rc.1" --force
+cargo install storm-cli --version "0.8.0-beta.1" --force
+cargo install store-cli --version "0.8.0" --force
+```
+
+```
+cargo install bp_node --version "0.8.0-alpha.2" --force
+#NOT OKE# cargo install lnp_node --path . --locked --all-features --version "0.$
+cargo install lnp_node --version "0.8.0" --force 
+cargo install rgb_node --version "0.8.0-rc.1" --force
+cargo install storm_node --version "0.8.0-beta.1" --force
+cargo install store_daemon --version "0.8.0" --force
